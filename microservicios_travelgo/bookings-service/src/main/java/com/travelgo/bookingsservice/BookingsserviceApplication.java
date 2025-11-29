2dbd61a947cfcf50db77729c3bf82e7b6efb5b58
Sebakm13 <seb.garridod@duocuc.cl>
2025-11-29 19:53:20 -0300

    Add files via upload

---
+++ microservicios_travelgo/bookings-service/src/main/java/com/travelgo/bookingsservice/BookingsserviceApplication.java
@@ -0,0 +1,7 @@
+package com.travelgo.bookingsservice;
+
+public class BookingsserviceApplication {
+    public static void main(String[] args) {
+        System.out.println("Microservicio Bookingsservice iniciado");
+    }
+}
