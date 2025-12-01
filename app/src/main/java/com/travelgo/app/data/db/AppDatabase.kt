8204516b45e78614b6e3dc1e7cb20405fd6b5dbe
Sebakm13 <seb.garridod@duocuc.cl>
2025-12-01 14:34:02 -0300

    Update AppDatabase.kt

--- app/src/main/java/com/travelgo/app/data/db/AppDatabase.kt
+++ app/src/main/java/com/travelgo/app/data/db/AppDatabase.kt
@@ -3,11 +3,10 @@ package com.travelgo.app.data.db
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.travelgo.app.data.dao.PaqueteDao
-import com.travelgo.app.data.db.PaqueteLocal
 
 @Database(
     entities = [PaqueteLocal::class],
-    version = 1,
+    version = 2,
     exportSchema = false
 )
 abstract class AppDatabase : RoomDatabase() {
