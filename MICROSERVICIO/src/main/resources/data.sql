6e7a462920ff986a570f105280d20c027b8ead46
sebaa <Se.aird@duocuc.cl>
2025-12-03 19:02:29 -0300

    Tests microservicios

--- MICROSERVICIO/src/main/resources/data.sql
+++ MICROSERVICIO/src/main/resources/data.sql
@@ -1,6 +1,7 @@
 INSERT INTO destinations (name, city, country, category, price_per_night, image_url, weather_summary)
 VALUES ('Hotel Playa Azul', 'Cancún', 'México', 'BEACH', 120.0, NULL, 'Soleado 29°');
 
+
 INSERT INTO destinations (name, city, country, category, price_per_night, image_url, weather_summary)
 VALUES ('Depto Centro', 'Santiago', 'Chile', 'CITY', 55.0, NULL, 'Parcialmente nublado 22°');
 
